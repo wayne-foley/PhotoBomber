f7425ef344bf3cf1b5a8eb41786104fc889d2980
Wayne Foley <whatthefoley@gmail.com>
2017-03-26 11:40:32 -0700

    refactored the call to flickr to use simple http request, and not use the bloated flickr lib

--- flickr/index.js
+++ flickr/index.js
@@ -1,35 +1,35 @@
-var Flickr = require('flickrapi');
+var phin = require('../phin');
 
-var getPhotoPage = function(pageNumber, user_id, flickr, callback) {
-  var searchOptions = {
-    user_id: user_id,
-    page: pageNumber,
-    per_page: 50,
-    extras: "url_t,url_m, url_l,url_o"
-  }
+var getPhotoPage = function(pageNumber, user_id, api_key, callback) {
+    var per_page = 50;
+    var extras = "url_t,url_m, url_l,url_o";
+    var flickrMethodUri = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json";
+    var uri = `${flickrMethodUri}&api_key=${api_key}&user_id=${user_id}&per_page=${per_page}&extras=${extras}&page=${pageNumber}`;
+    
+    phin( {"url": uri }, function(err, body, response) {
+        if (!err && response.statusCode == 200) {
+          var items = [];
+          var startPos = body.indexOf('({');
+          var endPos = body.indexOf('})');
+          var jsonString = body.substring(startPos+1, endPos+1);
+          var result = JSON.parse(jsonString);
 
-  flickr.photos.search(searchOptions, function(err, result) {
-    var items = [];
+          result.photos.photo.forEach(function(photo) {
+            var photoObject = {};
+            photoObject.id = photo.id;
+            photoObject.src = photo.url_l;
+            photoObject.downloadUrl =  photo.url_o;
+            photoObject.thumb = photo.url_m;
 
-    result.photos.photo.forEach(function(photo) {
-      var photoObject = {};
-      photoObject.id = photo.id;
-      photoObject.src = photo.url_l;
-      photoObject.downloadUrl =  photo.url_o;
-      photoObject.thumb = photo.url_m;
+            items.push(photoObject);
+          });
+          var ret = { totalPages: result.photos.pages, photos: items };
 
-      items.push(photoObject);
-    });
-    var ret = { totalPages: result.photos.pages, photos: items };
-
-    callback(false, ret);
-  });
+          callback(ret);
+        }
+    }); 
 };
 
 module.exports = function(flickrOptions, pageNumber, callback) {
-  Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-    getPhotoPage(pageNumber, flickrOptions.user_id, flickr, function(err, payload) {
-      callback(payload);
-    });
-  });
+    getPhotoPage(pageNumber, flickrOptions.user_id, flickrOptions.api_key, callback);
 }
